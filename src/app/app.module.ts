921f6e85a7638dbc5890a4986c8c12c3e7a0eee0
raja <sainiraja59@gmail.com>
2020-03-31 22:30:00 +0530

    old component deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './shared/nav/nav.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { HomeSliderComponent } from './shared/home-slider/home-slider.component';
-import { AboutSectionComponent } from './shared/about-section/about-section.component';
+
 import { FormsModule,ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent,
-    HeaderComponent,
-    HomeSliderComponent,
-    AboutSectionComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
